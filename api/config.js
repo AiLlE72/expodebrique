beaf9f131daecfe0f7553b712c92f8ffbd56990a
AiLlE72 <51365633+AiLlE72@users.noreply.github.com>
2020-03-01 00:28:22 +0100

    Delete config.js

--- api/config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    urlDBcloud : "mongodb+srv://archi:totocaca@cluster0-ctkdd.mongodb.net/test?retryWrites=true&w=majority",
-    urlDBlocal : "mongodb://localhost:27017/architecture",
-    
-}
