ccd0ff06a8f4894958828d2921aff66c90f8f1af
willy <willy.paris72@gmail.com>
2020-04-10 21:06:23 +0200

    correction cookies

--- api/controllers/cookie.js
+++ api/controllers/cookie.js
@@ -4,7 +4,7 @@ const cookieParser = require('cookie-parser')
 module.exports = {
     post: async (req, res) => {
         const date = new Date(new Date().setFullYear(new Date().getFullYear() + 1))
-        res.cookie('rememberToast', 'RT', { domain: 'localhost',path: '/', expires: date})
+        res.cookie('rememberToast', 'RT', { expires: date})
         res.redirect('back')
     }
 }
