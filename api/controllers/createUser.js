267199b400d97c261d9c966ba6ca7816e8bf0dce
willy <willy.paris72@gmail.com>
2020-04-23 21:27:57 +0200

    correction email verif

--- api/controllers/createUser.js
+++ api/controllers/createUser.js
@@ -47,7 +47,7 @@ module.exports = {
         // Nodemailer config  affectation des constantes declaré plus haut
         rand = Math.floor((Math.random() * 100) + 54) //crer un chiffre random
         host = req.get('host') // adresse du site hebergant l'envoi du mail de verif
-        link = "http://" + req.get('host') + "/verify/" + rand // construction du lien avec adresse du site et le chiffre random
+        link = "https://expodebrique.willyparis.fr/verify/" + rand // construction du lien avec adresse du site et le chiffre random
         mailOptions = {
             from: key.mailUser, // adresse du mail qui envoi le lien de verif
             to: req.body.email, // adresse de la personne qui s'inscrit
@@ -95,8 +95,10 @@ module.exports = {
                 (error, post) => {
                     if (error) {
                         res.send(error)
-                    } else {
+                    } else {                        
                         // Nodemailer transport 
+                        console.log(host);
+                        
                         transporter.sendMail(mailOptions, (err, res, next) => { // utilisation de la constante transporter et de la fonction d'envoi de mail
                             if (err) {
                                 res.send(err)
