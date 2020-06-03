5f3e771d6b43a4e047674c675a4d286dc6f8072e
willy <willy.paris72@gmail.com>
2020-06-04 00:10:21 +0200

    ajout google analytics

--- api/controllers/cookie.js
+++ api/controllers/cookie.js
@@ -4,6 +4,12 @@ const cookieParser = require('cookie-parser')
 module.exports = {
     post: async (req, res) => {
         const date = new Date(new Date().setFullYear(new Date().getFullYear() + 1))
+        if(req.body.cookieGA){
+            res.cookie('rememberGA', 'GoogAnal',{ expires: date} )
+        } else {
+            res.clearCookie('rememberGA')
+        }
+        
         res.cookie('rememberToast', 'RT', { expires: date})
         res.redirect('back')
     }
